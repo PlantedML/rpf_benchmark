eb4cb25e21fc3933e91f9a5f502a9d8d3d481d5d
Lukas Burk <burk@leibniz-bips.de>
2022-04-07 15:12:02 +0200

    Minor tweaks

--- batchmark.R
+++ batchmark.R
@@ -74,7 +74,8 @@ reg_dir <- here::here("registry", reg_name)
 dir.create(here::here("registry"))
 
 if (dir.exists(reg_dir)) {
-  reg = loadRegistry(reg_dir, writeable = TRUE)
+  unlink(reg_dir)
+  #reg = loadRegistry(reg_dir, writeable = TRUE)
 } else {
   reg = makeExperimentRegistry(reg_dir, seed = 230749)
 }
@@ -104,40 +105,41 @@ if (grepl("node\\d{2}|bipscluster", system("hostname", intern = TRUE))) {
   submitJobs()
 }
 
-
 waitForJobs()
 getStatus()
 
 bmr <- reduceResultsBatchmark()
 saveRDS(bmr, "bmr.Rds")
 
-# Aggregate
-aggr <- bmr$aggregate(measures = mymsr)
-
-# Only tasks for which we have results of all three learners
-completed_tasks <- aggr[, names(which(table(task_id) == 3))]
-aggr <- aggr[task_id %in% completed_tasks, ]
-
-# Plot over individual datasets
-bmr$filter(task_ids = completed_tasks)
-scores <- bmr$score(measures = mymsr)
-scores[, learner_name := gsub("\\.tuned", "", gsub("classif\\.", "", learner_id))]
-scores[, task_name := gsub(" \\(Supervised Classification\\)", "", gsub("Task \\d+: ", "", task_id))]
-ggplot(scores, aes(x = learner_name, y = classif.auc)) +
-  facet_wrap(~ task_name) +
-  geom_boxplot() +
-  geom_point() +
-  theme_bw()
-ggsave("bmr.pdf")
-
-autoplot(bmr, measure = mymsr) +
-  theme_bw()
-ggsave("bmr.pdf")
-
-# Plot over all datasets
-aggr[, learner_name := gsub("\\.tuned", "", gsub("classif\\.", "", learner_id))]
-ggplot(aggr, aes(x = learner_name, y = classif.auc)) +
-  geom_boxplot() +
-  geom_point() +
-  theme_bw()
-ggsave("aggr.pdf")
+if (FALSE) {
+  # Aggregate
+  aggr <- bmr$aggregate(measures = mymsr)
+  
+  # Only tasks for which we have results of all three learners
+  completed_tasks <- aggr[, names(which(table(task_id) == 3))]
+  aggr <- aggr[task_id %in% completed_tasks, ]
+  
+  # Plot over individual datasets
+  bmr$filter(task_ids = completed_tasks)
+  scores <- bmr$score(measures = mymsr)
+  scores[, learner_name := gsub("\\.tuned", "", gsub("classif\\.", "", learner_id))]
+  scores[, task_name := gsub(" \\(Supervised Classification\\)", "", gsub("Task \\d+: ", "", task_id))]
+  ggplot(scores, aes(x = learner_name, y = classif.auc)) +
+    facet_wrap(~ task_name) +
+    geom_boxplot() +
+    geom_point() +
+    theme_bw()
+  ggsave("bmr.pdf")
+  
+  autoplot(bmr, measure = mymsr) +
+    theme_bw()
+  ggsave("bmr.pdf")
+  
+  # Plot over all datasets
+  aggr[, learner_name := gsub("\\.tuned", "", gsub("classif\\.", "", learner_id))]
+  ggplot(aggr, aes(x = learner_name, y = classif.auc)) +
+    geom_boxplot() +
+    geom_point() +
+    theme_bw()
+  ggsave("aggr.pdf")
+}
