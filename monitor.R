4887d9429384251b596e8aa841a0484b1bde9e80
Lukas Burk <burk@leibniz-bips.de>
2022-09-03 15:03:57 +0200

    Prettification

--- monitor.R
+++ monitor.R
@@ -6,17 +6,25 @@ loadRegistry(reg_dir, writeable = FALSE)
 
 
 # Running -----------------------------------------------------------------
+cli::cli_h1("Running")
 getStatus()
-unwrap(getJobTable(findRunning()))
 
+cat("\n")
+
+tbl_running <- unwrap(getJobTable(findRunning()))
+if (nrow(tbl_running) > 0) {
+  tbl_running[, c("job.id", "time.running", "task_id", "learner_id")]
+  tbl_running[, .(count = .N), by = learner_id]
+}
 
 # Done --------------------------------------------------------------------
+cli::cli_h1("Done")
 tbl_done <- unwrap(getJobTable(findDone()))
-
-tbl_done[, c("job.id", "time.running", "task_id", "learner_id")]
-
+tbl_done <- tbl_done[, c("job.id", "time.running", "task_id", "learner_id")]
 tbl_done[, .(count = .N), by = learner_id]
 
+cat("\n")
 
 # Error'd -----------------------------------------------------------------
+cli::cli_h1("Errors")
 getErrorMessages(findErrors())
