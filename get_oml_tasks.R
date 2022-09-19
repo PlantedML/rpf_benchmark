d3bf764fe1e3c11515b5bfedf9f0e47efb51bb9a
Lukas Burk <lukas@quantenbrot.de>
2022-09-19 17:16:30 +0200

    Select more tasks but limit by n * p

--- get_oml_tasks.R
+++ get_oml_tasks.R
@@ -8,36 +8,45 @@ options("mlr3oml.cache" = TRUE) # requires install.packages('qs')
 # Get CC18 tasks
 task_ids <- getOMLStudy('OpenML-CC18')$tasks$task.id
 
-# Select only binary classification with numeric input
-task_info <- sapply(task_ids, function(task_id) {
-  task <- tsk("oml", task_id)
-  c(twoclass = ("twoclass" %in% task$properties),
-    featuretypes = all(task$feature_types$type %in% c("integer", "numeric", "factor")), # disallow logical
-    nomissing = (max(task$missings()) == 0))
-})
-
-task_ids_selected <- task_ids[task_info[1, ] & task_info[2, ] & task_info[3, ]]
-
-# All feature types in set
+# Show all feature types in set: There are only numeric, integer + factor, no need to select, technically
 # sapply(task_ids, \(x) unique(tsk("oml", x)$feature_types$type)) |> unlist() |> unique()
 
+# Get task summary for selection and later display
+task_info <- do.call(rbind, lapply(task_ids, function(task_id) {
+  task <- tsk("oml", task_id)
 
-#dput(task_ids_selected)
-# task_ids_selected <- c(37L, 43L, 3902L, 3903L, 3913L, 3917L, 3918L, 9910L, 9946L,
-#                        9952L, 9957L, 9976L, 9978L, 10093L, 10101L, 146819L, 146820L,
-#                        167120L)
+  # Clean up name
+  task_name <- gsub(pattern = "\\s\\(Supervised\\ Classification\\)", "", task$id)
+  task_name <- gsub(pattern = "Task \\d+: ", "", task_name)
 
-#tasks <- lapply(task_ids_selected[c(1, 3, 5)], tsk, .key = "oml")
-tasks <- lapply(task_ids_selected, tsk, .key = "oml")
+  task_n <- task$nrow
+  task_p <- length(task$feature_names)
 
-task_summary <- do.call(rbind, lapply(tasks, function(tsk) {
   data.frame(
-    task_name = sub(pattern = "\\s\\(Supervised\\ Classification\\)", "", tsk$id),
-    n = tsk$nrow,
-    p = length(tsk$feature_names)
+    task_id = task_id,
+    task_name = task_name,
+    twoclass = ("twoclass" %in% task$properties),
+    featuretypes = all(task$feature_types$type %in% c("integer", "numeric", "factor")), # disallow logical
+    nomissing = (max(task$missings()) == 0),
+    n = task_n,
+    p = task_p,
+    dim = task_n * task_p
   )
 }))
-task_summary$task_id <- task_ids_selected
-task_summary <- task_summary[order(task_summary$n * task_summary$p, decreasing = TRUE), ]
 
-saveRDS("task_summary.rds")
+# Select for required properties
+task_info <- subset(task_info, twoclass & featuretypes & nomissing)
+
+# Rank & sort by dimensionality
+task_info$dim_rank <- rank(task_info$dim)
+task_info <- task_info[order(task_info$dim_rank), ]
+
+# Rough heuristic: n * p smaller than 1.000.000, gets 23 tasks in this case.
+task_info <- subset(task_info, dim <= 1e6)
+task_ids_selected <- task_info[["task_id"]]
+
+# This object is what counts, as it is used in batchmark.R
+tasks <- lapply(task_ids_selected, tsk, .key = "oml")
+
+# Save for later reference
+saveRDS(task_info, "task_summary.rds")
