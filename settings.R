829af0f622d681c86c4b98e92f46a73a74a0eef0
Lukas Burk <lukas@quantenbrot.de>
2023-01-13 17:37:08 +0100

    Adjust rpf tuning space for extended rerun

--- settings.R
+++ settings.R
@@ -10,8 +10,8 @@ tuning_budget <- 200 # n_evals, 50 for trial mode
 rpf.ntrees <- 50
 rpf.maxintlim <- 30
 rpf.maxintratio <- p_dbl(0, 1)
-rpf.loss <- p_fct(c("L1", "exponential")) # removed logit due to runtime
-rpf.splits <- p_int(10, 50) # Bumped to lower = 10 as per Munir
+rpf.loss <- p_fct(c("L1", "L2", "exponential")) # removed logit due to runtime
+rpf.splits <- p_int(10, 100) # Bumped to lower = 10 and upper = 100 as per Munir
 rpf.split_try <- p_int(1, 20)
 rpf.t_try <- p_dbl(0.1, 1)
 
