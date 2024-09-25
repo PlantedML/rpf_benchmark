daac490ca9cfc9316afed96717833fa7054cdcad
Lukas Burk <burk@leibniz-bips.de>
2024-09-25 15:54:47 +0200

    fix encapsulate

--- learners.R
+++ learners.R
@@ -19,7 +19,6 @@ if (conf$tuning$tuner == "mbo") {
 
 wrap_autotuner <- function(learner_id, ..., search_space, .encode = FALSE, .tuning_measure) {
   cli::cli_alert_info("Setting up {.val {learner_id}} with {.val {(.tuning_measure)}}")
-  paradox::assert_param_set(search_space)
 
   base_learner <- lrn(learner_id, predict_type = "prob", ...)
 
@@ -39,9 +38,9 @@ wrap_autotuner <- function(learner_id, ..., search_space, .encode = FALSE, .tuni
 
 
   if (conf$fallback$inner) {
-    # base_learner$encapsulate("callr", lrn("classif.featureless"))
-    base_learner$fallback = lrn("classif.featureless", predict_type = "prob")
-    base_learner$encapsulate = c(train = "callr", predict = "callr")
+    suppressWarnings(base_learner$encapsulate("callr", lrn("classif.featureless")))
+    # base_learner$fallback = lrn("classif.featureless", predict_type = "prob")
+    # base_learner$encapsulate = c(train = "callr", predict = "callr")
   }
 
   base_learner$timeout = c(train   = conf$timeout$base$train  * 3600,
@@ -72,9 +71,9 @@ wrap_autotuner <- function(learner_id, ..., search_space, .encode = FALSE, .tuni
   )
 
   if (conf$fallback$outer) {
-    # at$encapsulate("callr", lrn("classif.featureless"))
-    at$fallback = lrn("classif.featureless", predict_type = "prob")
-    at$encapsulate = c(train = "callr", predict = "callr")
+    suppressWarnings(at$encapsulate("callr", lrn("classif.featureless")))
+    # at$fallback = lrn("classif.featureless", predict_type = "prob")
+    # at$encapsulate = c(train = "callr", predict = "callr")
   }
 
   at$timeout = c(train   = conf$timeout$autotuner$train  * 3600,
